09c00da328b4e82e957a480c1437813f6aeabe29
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-04-12 16:44:52 +0700

    Shape manipulations for RectangularCustomView.kt

--- app/src/main/java/com/avp/customviews/customviews/RectangularCustomView.kt
+++ app/src/main/java/com/avp/customviews/customviews/RectangularCustomView.kt
@@ -1,6 +1,5 @@
-package com.avp.customviews.rectangular
+package com.avp.customviews.customviews
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -12,10 +11,12 @@ import android.util.AttributeSet
 import android.view.View
 import com.avp.customviews.R
 
+
 class RectangularCustomView : View {
     private lateinit var mPaint: Paint
     private lateinit var mRect: Rect
     private var mSquareColor: Int = 0
+    private var mPadding: Int = 0
 
     constructor(context: Context?) : super(context) {
         init(null)
@@ -45,22 +46,39 @@ class RectangularCustomView : View {
         if (set == null) return
 
         val ta = context.obtainStyledAttributes(set, R.styleable.RectangularCustomView)
-        mSquareColor = ta.getColor(R.styleable.RectangularCustomView_square_color, Color.GREEN)
+        mSquareColor = ta.getColor(com.avp.customviews.R.styleable.RectangularCustomView_square_color, Color.GREEN)
         ta.recycle()
+    }
+
+    fun swapColor() {
+        mPaint.color = if (mPaint.color == mSquareColor) Color.RED else mSquareColor
+        postInvalidate()
+    }
+
+    fun customPaddingUp(padding: Int) {
+        mPadding += padding
+        postInvalidate()
+
+    }
+
+    fun customPaddingDown(padding: Int) {
+        mPadding -= padding
+        postInvalidate()
     }
 
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        /*
-        * Avoid object allocations during draw/layout operations (preallocate and reuse instead) less... (Ctrl+F1)
-        * Inspection info:You should avoid allocating objects during a drawing or layout operation. These are called frequently,
-        * so a smooth UI can be interrupted by garbage collection pauses caused by the object allocations.
-        * The way this is generally handled is to allocate the needed objects up front and to reuse them for each drawing operation.
-        * Some methods allocate memory on your behalf (such as Bitmap.create), and these should be handled in the same way.
-        * Issue id: DrawAllocation
-        * */
+        /**
+         * Avoid object allocations during draw/layout operations (preallocate and reuse instead) less... (Ctrl+F1)
+         * Inspection info:You should avoid allocating objects during a drawing or layout operation. These are called frequently,
+         * so a smooth UI can be interrupted by garbage collection pauses caused by the object allocations.
+         * The way this is generally handled is to allocate the needed objects up front and to reuse them for each drawing operation.
+         * Some methods allocate memory on your behalf (such as Bitmap.create), and these should be handled in the same way.
+         * Issue id: DrawAllocation
+         * */
+
 //        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 //        paint.color = Color.GREEN
 //        val rect = Rect()
@@ -69,12 +87,17 @@ class RectangularCustomView : View {
 //        rect.top = 0
 //        rect.bottom = height
 
-
+        /**
+         * Left is x axis
+         * Top is y axis
+         * Right is width of view
+         * bottom is height of view
+         * */
         mPaint.color = Color.GREEN
-        mRect.left = 0
-        mRect.right = width
-        mRect.top = 0
-        mRect.bottom = height
+        mRect.left = 0 + mPadding
+        mRect.right = width -mPadding
+        mRect.top = 0 + mPadding
+        mRect.bottom = height - mPadding
 
         canvas?.drawRect(mRect, mPaint)
     }
--- app/src/main/java/com/avp/customviews/rectangular/RectangularActivity.kt
+++ app/src/main/java/com/avp/customviews/rectangular/RectangularActivity.kt
@@ -2,11 +2,27 @@ package com.avp.customviews.rectangular
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.avp.customviews.R
+import kotlinx.android.synthetic.main.activity_rectangular.*
 
 class RectangularActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rectangular)
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.one -> {
+                rectangularCustomView.customPaddingUp(30)
+            }
+            R.id.two -> {
+                rectangularCustomView.swapColor()
+            }
+            R.id.three -> {
+                rectangularCustomView.customPaddingDown(30)
+            }
+        }
     }
 }
