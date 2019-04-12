237f1b45df924ec3d1730832a59c2760a2ccd376
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-04-12 13:35:22 +0700

    Custom view with attrs.xml

--- app/src/main/java/com/avp/customviews/rectangular/RectangularCustomView.kt
+++ app/src/main/java/com/avp/customviews/rectangular/RectangularCustomView.kt
@@ -10,8 +10,12 @@ import android.os.Build
 import android.support.annotation.RequiresApi
 import android.util.AttributeSet
 import android.view.View
+import com.avp.customviews.R
 
 class RectangularCustomView : View {
+    private lateinit var mPaint: Paint
+    private lateinit var mRect: Rect
+    private var mSquareColor: Int = 0
 
     constructor(context: Context?) : super(context) {
         init(null)
@@ -35,21 +39,44 @@ class RectangularCustomView : View {
     }
 
     private fun init(set: AttributeSet?) {
+        mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        mRect = Rect()
 
+        if (set == null) return
+
+        val ta = context.obtainStyledAttributes(set, R.styleable.RectangularCustomView)
+        mSquareColor = ta.getColor(R.styleable.RectangularCustomView_square_color, Color.GREEN)
+        ta.recycle()
     }
 
-    @SuppressLint("DrawAllocation")
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        paint.color = Color.GREEN
-        val rect = Rect()
-        rect.left = 0
-        rect.right = width
-        rect.top = 0
-        rect.bottom = height
 
-        canvas?.drawRect(rect, paint)
+        /*
+        * Avoid object allocations during draw/layout operations (preallocate and reuse instead) less... (Ctrl+F1)
+        * Inspection info:You should avoid allocating objects during a drawing or layout operation. These are called frequently,
+        * so a smooth UI can be interrupted by garbage collection pauses caused by the object allocations.
+        * The way this is generally handled is to allocate the needed objects up front and to reuse them for each drawing operation.
+        * Some methods allocate memory on your behalf (such as Bitmap.create), and these should be handled in the same way.
+        * Issue id: DrawAllocation
+        * */
+//        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+//        paint.color = Color.GREEN
+//        val rect = Rect()
+//        rect.left = 0
+//        rect.right = width
+//        rect.top = 0
+//        rect.bottom = height
+
+
+        mPaint.color = Color.GREEN
+        mRect.left = 0
+        mRect.right = width
+        mRect.top = 0
+        mRect.bottom = height
+
+        canvas?.drawRect(mRect, mPaint)
     }
 
 }
